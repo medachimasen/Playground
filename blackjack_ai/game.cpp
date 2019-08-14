ce40ee25d688d29c1022c82d092b87f333de8500
Benjamin Cheong <bcheong@azelf.cs.uchicago.edu>
2019-08-13 21:58:19 -0500

    game.cpp edited to finish most of player

--- blackjack_ai/game.cpp
+++ blackjack_ai/game.cpp
@@ -3,31 +3,29 @@
 using namespace std;
 class Player{
 public:
-	Player(Card a){
+	Player(int mon){
+		money = mon;
+	}
+	void bet(int a){
+		money -= a;
+	}
+	void firstDeal(Card a, Card b){
 		hand.push_back(a);
-		int val = a.get_val();
-		if(val > 1 && val < 11){
-			total = val;
-			aceTotal = val;
-		}
-		else if (val >= 11){
-			total = 10;
-			aceTotal = 10;
-		}
-		else{
-			total = 1;
-			aceTotal = 11;
-		}
+		hand.push_back(b);
+		total = changeToBJVal(a) + changeToBJVal(b);
 		bust = 0;
+		if(total == 21){
+			bj = 1;
+		}
 	}
 	void show(){
-		if(aceTotal <= 21 && aceTotal != total){
-			cout << "Total: " << total << " or " << aceTotal << endl;
+		if(total <= 10 && aces > 0){
+			cout << "Total: " << total - 10 << " or " << total << endl;
 		}
 		else{
 			cout << "Total: " << total << endl;
 		}
-		for(int i = 0; i < hand.size(); I++){
+		for(int i = 0; i < hand.size(); i++){
 			hand[i].show();
 		}
 
@@ -40,17 +38,48 @@ public:
 	}
 	void addCard(Card a){
 		hand.push_back(a);
-		int 
+		int val = changeToBJVal(a);
+		total += val;
+		if (val > 21){
+			if(aces > 0){
+				total -= 10;
+			}
+			else{
+				bust = 1;
+			}
+		}
 	}
 	void addMoney(int a){
-
+		money += a;
 	}
-	void reset();
+	void reset(){
+		hand = new vector<Card>;
+		bj = 0;
+		aces = 0;
+	};
 private:
 	vector<Card> hand;
 	int total;
-	int aceTotal;
 	int money;
-	
 	bool bust;
+	bool bj;
+	int aces;
+}
+int changeToBJVal(Card a){
+	int val = a.get_val();
+	if(val > 1 && val < 11){
+		return val;
+	}
+	else if (val >= 11){
+		return 10;
+	}
+	else{
+		return 11;
+	}
+}
+bool isAce(Card a){ //possible parallel implementation of search here
+	if (a.get_val == 1){
+		return true;
+	}
+	return false;
 }
